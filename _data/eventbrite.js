28b3dfc65b601286e553ed7a0b8815bdc64022f2
Chris Bracco <chris@cbracco.me>
2024-06-20 11:54:40 -0400

    Add Eventbrite API

---
+++ _data/eventbrite.js
@@ -0,0 +1,17 @@
+const EleventyFetch = require('@11ty/eleventy-fetch');
+
+module.exports = async function () {
+    try {
+        let url = `https://www.eventbriteapi.com/v3/organizations/2048015915213/events/?token=${process.env.EVENTBRITE_API_KEY}&expand=ticket_classes`;
+
+        let json = EleventyFetch(url, {
+            duration: '1d',
+            type: 'json',
+            verbose: true,
+        });
+
+        return json;
+    } catch (error) {
+        return error;
+    }
+};
